52d41a83ff9c9d45cf689bef54a5b93fa4c2d007
Alex Klick <aklick12@gmail.com>
2021-07-20 17:49:30 -0600

    passes story 12

--- app/controllers/pet_apps_controller.rb
+++ app/controllers/pet_apps_controller.rb
@@ -5,6 +5,7 @@ class PetAppsController < ApplicationController
       redirect_to "/apps/#{pet_app_params[:app_id]}"
     end
   end
+
   private
   def pet_app_params
     params.permit(:app_id, :pet_id)
--- app/models/pet_app.rb
+++ app/models/pet_app.rb
@@ -1,7 +1,7 @@
 class PetApp < ApplicationRecord
   belongs_to :pet
   belongs_to :app
-
+  enum status: { pending: 0, approved: 1, rejected: 2 }
   validates :pet_id, presence: true
   validates :app_id, presence: true
 end
